18fbcc06196d5c574d282f5c6cb0c706e917e309
Ahnaf Bappy <ahnafhsn@gmail.com>
2022-01-08 00:53:16 +0600

    undefine clear

---
+++ undefine.js
@@ -0,0 +1,16 @@
+let pakhi;
+
+function add (num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add (num1,  num2){
+    console.log(num1, num2);
+}
+
+const result = add( 13, 82);
+console.log(result);
+
+let ages = [1,2,3,4,5];
+console.log(ages[11]);
