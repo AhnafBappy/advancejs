92b8955b5c96e82fcaee189b6a1871aa427b3798
Ahnaf Bappy <ahnafhsn@gmail.com>
2022-01-08 00:35:42 +0600

    this is true false statement

---
+++ map-filter.js
@@ -0,0 +1,25 @@
+const numbers = [3, 4, 5, 6, 7, 8];
+// const output = [];
+// for(let i = 0; i<numbers.length; i++){
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+// const squre = x => x * x;
+
+// const result = numbers.map(function(element){
+//     return element * element;
+// })
+// console.log(result);
+
+const result = numbers.map(x => x * x)
+console.log(result);
+
+function age (robin){
+    var noy = 5;
+    var newAge = noy * age;
+    return age;
+}
+var robin = 5;
+console.log(age());
---
+++ true-false.js
@@ -0,0 +1,7 @@
+const nam = " ";
+if (nam.length > 0){
+    console.log("This condition is true");
+}
+else{
+    console.log("condition is false")
+};
